a60906268179b9d5053f8e4496ab805f968a5c9b
unknown <afanhanan10@gmail.com>
2024-02-16 10:45:15 +0700

    DataTable NextUI Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,133 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import { faCaretDown, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input, Pagination, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, getKeyValue } from '@nextui-org/react';
+import React, { useMemo, useState } from 'react';
+
+export const generateHeaderTable = (col: any[]) => {
+  const oldCol = [
+    {
+      key: 'no',
+      label: 'NO',
+    },
+  ];
+
+  const newCol = col.map((column, i) => {
+    return {
+      key: column.toString().toLocaleLowerCase().replaceAll(' ', '_'),
+      label: column,
+    };
+  });
+  return [...oldCol, ...newCol];
+};
+
+interface DataTable {
+  columns: any[];
+  data?: any[];
+  isLoading?: any;
+  searchPlaceholder?: string;
+  isSearchable?: boolean;
+  searchFilterColumn?: string[];
+  extraContent?: JSX.Element;
+}
+
+export default function DataTable({ columns, data = [], isLoading = false, extraContent, isSearchable = false, searchFilterColumn = [] }: DataTable) {
+  const columnsFinal: any[] = generateHeaderTable(columns);
+  const [page, setPage] = useState(1);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [search, setSearch] = useState('');
+
+  const pages = Math.ceil(data.length / rowsPerPage);
+
+  const filtered = (filter: any) => {
+    let result = true;
+
+    if (isSearchable) {
+      searchFilterColumn.every((filterColumn: string) => {
+        const searchFilter = filterColumn.toLocaleLowerCase().replaceAll(' ', '_');
+        const regex = new RegExp(`${search}`, 'gi');
+        result = regex.test(filter[searchFilter]);
+        if (!result) return true;
+      });
+    }
+
+    return result;
+  };
+
+  const generateSearchPlaceHolder = () => {
+    if (searchFilterColumn.length === 0) return 'Search something...';
+
+    let text = 'Search by ';
+
+    searchFilterColumn.forEach((filterColumn, i) => {
+      if (i === 0) return (text += filterColumn);
+
+      return (text += ' or ' + filterColumn);
+    });
+
+    return text;
+  };
+
+  const TopContent = () => {
+    return (
+      <div className="flex justify-between items-center">
+        {isSearchable && <Input type="search" className="w-1/2" value={search} onValueChange={(val) => setSearch(val)} size={'md'} placeholder={generateSearchPlaceHolder()} startContent={<FontAwesomeIcon icon={faSearch} />} />}
+        {extraContent}
+      </div>
+    );
+  };
+
+  const BottomContent = () => {
+    if (pages === 0) return;
+    return (
+      <div className="flex items-center justify-between">
+        <Dropdown>
+          <DropdownTrigger>
+            <Button variant={'light'} endContent={<FontAwesomeIcon icon={faCaretDown} />}>
+              Rows per Page : {rowsPerPage}
+            </Button>
+          </DropdownTrigger>
+          <DropdownMenu onAction={(key) => setRowsPerPage(Number(key))}>
+            <DropdownItem key={'10'}>10</DropdownItem>
+            <DropdownItem key={'25'}>25</DropdownItem>
+            <DropdownItem key={'50'}>50</DropdownItem>
+            <DropdownItem key={'100'}>100</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <Pagination showControls isCompact page={page} total={pages} onChange={(page) => setPage(page)} />
+      </div>
+    );
+  };
+
+  const dataFinal = useMemo(() => {
+    let finalPage = page - 1;
+    if (pages < page && data.length !== 0) {
+      finalPage = 0;
+      setPage(1);
+    }
+    const start = finalPage * rowsPerPage;
+    const end = start + rowsPerPage;
+
+    const filteredData = data.filter(filtered);
+    return filteredData.slice(start, end);
+  }, [search, data, page, pages]);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <Table aria-label="Accounting Table" className="my-5" bottomContentPlacement={'outside'} topContentPlacement={'outside'} topContent={TopContent()} bottomContent={BottomContent()}>
+      <TableHeader columns={columnsFinal}>
+        {(column) => (
+          <TableColumn className="" aria-label={column.key} key={column.key}>
+            {column.label}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody items={dataFinal} emptyContent={'No Data Display'} isLoading={isLoading} loadingContent={'Loading Data...'}>
+        {(item: any) => (
+          <TableRow aria-label={item.key} key={item.key} className="">
+            {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
   );
 }
